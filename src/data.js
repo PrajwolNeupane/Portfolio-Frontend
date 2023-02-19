cc197177910928f9fc5e0a56291e226740eab1f5
Prajwol <96991442+PrajwolNeupane@users.noreply.github.com>
2023-02-19 17:01:11 +0000

    Hey

--- src/data.js
+++ src/data.js
@@ -7,5 +7,68 @@ export const projectData = [
         image: "https://firebasestorage.googleapis.com/v0/b/portfolio-backend-64b7e.appspot.com/o/1670605660455-Project-10.png?alt=media&token=8e7651ae-aa6f-44ed-acdb-2b5cc584cfa3",
         link: "https://corona-tracker-77ae1.web.app/"
 
+    },
+    {
+        id: 1,
+        name: "Corona Tracker",
+        description: "Corona Tracker is a website design that gives live count and information about the corona virus. This website provides a wide range of information such as the total number of tests, case rate, death rate, and recovery rate.While building this project, I learn how to fetch data from API. I use Firebase for user authentication and also to save messages in firebase firestore. In order to make Proper UI, I use Material UI and also Chart JS, and react-chart-2 for Line Chart.",
+        tagline: "React,Firebase,Chart JS,Material UI",
+        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-backend-64b7e.appspot.com/o/1670605660455-Project-10.png?alt=media&token=8e7651ae-aa6f-44ed-acdb-2b5cc584cfa3",
+        link: "https://corona-tracker-77ae1.web.app/"
+
+    },
+    {
+        id: 1,
+        name: "Corona Tracker",
+        description: "Corona Tracker is a website design that gives live count and information about the corona virus. This website provides a wide range of information such as the total number of tests, case rate, death rate, and recovery rate.While building this project, I learn how to fetch data from API. I use Firebase for user authentication and also to save messages in firebase firestore. In order to make Proper UI, I use Material UI and also Chart JS, and react-chart-2 for Line Chart.",
+        tagline: "React,Firebase,Chart JS,Material UI",
+        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-backend-64b7e.appspot.com/o/1670605660455-Project-10.png?alt=media&token=8e7651ae-aa6f-44ed-acdb-2b5cc584cfa3",
+        link: "https://corona-tracker-77ae1.web.app/"
+
+    },
+    {
+        id: 1,
+        name: "Corona Tracker",
+        description: "Corona Tracker is a website design that gives live count and information about the corona virus. This website provides a wide range of information such as the total number of tests, case rate, death rate, and recovery rate.While building this project, I learn how to fetch data from API. I use Firebase for user authentication and also to save messages in firebase firestore. In order to make Proper UI, I use Material UI and also Chart JS, and react-chart-2 for Line Chart.",
+        tagline: "React,Firebase,Chart JS,Material UI",
+        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-backend-64b7e.appspot.com/o/1670605660455-Project-10.png?alt=media&token=8e7651ae-aa6f-44ed-acdb-2b5cc584cfa3",
+        link: "https://corona-tracker-77ae1.web.app/"
+
+    },
+    {
+        id: 1,
+        name: "Corona Tracker",
+        description: "Corona Tracker is a website design that gives live count and information about the corona virus. This website provides a wide range of information such as the total number of tests, case rate, death rate, and recovery rate.While building this project, I learn how to fetch data from API. I use Firebase for user authentication and also to save messages in firebase firestore. In order to make Proper UI, I use Material UI and also Chart JS, and react-chart-2 for Line Chart.",
+        tagline: "React,Firebase,Chart JS,Material UI",
+        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-backend-64b7e.appspot.com/o/1670605660455-Project-10.png?alt=media&token=8e7651ae-aa6f-44ed-acdb-2b5cc584cfa3",
+        link: "https://corona-tracker-77ae1.web.app/"
+
+    },
+    {
+        id: 1,
+        name: "Corona Tracker",
+        description: "Corona Tracker is a website design that gives live count and information about the corona virus. This website provides a wide range of information such as the total number of tests, case rate, death rate, and recovery rate.While building this project, I learn how to fetch data from API. I use Firebase for user authentication and also to save messages in firebase firestore. In order to make Proper UI, I use Material UI and also Chart JS, and react-chart-2 for Line Chart.",
+        tagline: "React,Firebase,Chart JS,Material UI",
+        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-backend-64b7e.appspot.com/o/1670605660455-Project-10.png?alt=media&token=8e7651ae-aa6f-44ed-acdb-2b5cc584cfa3",
+        link: "https://corona-tracker-77ae1.web.app/"
+
+    },
+    {
+        id: 1,
+        name: "Corona Tracker",
+        description: "Corona Tracker is a website design that gives live count and information about the corona virus. This website provides a wide range of information such as the total number of tests, case rate, death rate, and recovery rate.While building this project, I learn how to fetch data from API. I use Firebase for user authentication and also to save messages in firebase firestore. In order to make Proper UI, I use Material UI and also Chart JS, and react-chart-2 for Line Chart.",
+        tagline: "React,Firebase,Chart JS,Material UI",
+        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-backend-64b7e.appspot.com/o/1670605660455-Project-10.png?alt=media&token=8e7651ae-aa6f-44ed-acdb-2b5cc584cfa3",
+        link: "https://corona-tracker-77ae1.web.app/"
+
+    },
+    {
+        id: 1,
+        name: "Corona Tracker",
+        description: "Corona Tracker is a website design that gives live count and information about the corona virus. This website provides a wide range of information such as the total number of tests, case rate, death rate, and recovery rate.While building this project, I learn how to fetch data from API. I use Firebase for user authentication and also to save messages in firebase firestore. In order to make Proper UI, I use Material UI and also Chart JS, and react-chart-2 for Line Chart.",
+        tagline: "React,Firebase,Chart JS,Material UI",
+        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-backend-64b7e.appspot.com/o/1670605660455-Project-10.png?alt=media&token=8e7651ae-aa6f-44ed-acdb-2b5cc584cfa3",
+        link: "https://corona-tracker-77ae1.web.app/"
+
     }
 ]
